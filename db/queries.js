90c0a81fb4733e6ecb75d4e2e6057ef0726d9a8c
Ari <=>
2023-08-11 23:16:08 -0700

    Changed postgres table column names to camelCase and removd table name from id names

--- db/queries.js
+++ db/queries.js
@@ -4,31 +4,31 @@ async function createTables() {
     const createTablesQuery = 
     `
     CREATE TABLE IF NOT EXISTS users (
-        user_id SERIAL PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         email TEXT UNIQUE,
         password TEXT
     );
 
     CREATE TABLE IF NOT EXISTS feeds (
-        feed_id SERIAL PRIMARY KEY,
-        feed_url TEXT UNIQUE,
-        icon_url TEXT,
+        id SERIAL PRIMARY KEY,
+        feedUrl TEXT UNIQUE,
+        iconUrl TEXT,
         title TEXT
     );
 
     CREATE TABLE IF NOT EXISTS subscriptions (
-        subscription_id SERIAL PRIMARY KEY,
-        user_id INT REFERENCES users(user_id),
-        feed_id INT REFERENCES feeds(feed_id)
+        id SERIAL PRIMARY KEY,
+        userId INT REFERENCES users(user_id),
+        feedId INT REFERENCES feeds(feed_id)
     );
 
     CREATE TABLE IF NOT EXISTS posts (
-        post_id SERIAL PRIMARY KEY,
-        feed_id INT REFERENCES feeds(feed_id),
+        id SERIAL PRIMARY KEY,
+        feedId INT REFERENCES feeds(feed_id),
         title TEXT,
         url TEXT,
         comments_url TEXT,
-        media_url TEXT,
+        mediaUrl TEXT,
         date TIMESTAMPTZ
     );
     `;
@@ -38,77 +38,66 @@ async function createTables() {
 async function getUserSubscriptions(userId) {
     const getSubscriptionsQuery = 
     `
-    SELECT feed_id, feed_url, icon_url, title
-    FROM feeds JOIN subscriptions ON feeds.feed_id = subscriptions.feed_id
-    WHERE user_id = $1;
+    SELECT feed.id AS feedId, feedUrl, iconUrl, title
+    FROM feeds JOIN subscriptions ON feeds.id = subscriptions.feedId
+    WHERE userId = $1;
     `;
-    try {
-        return await query(getSubscriptionsQuery, [userId]);
-    } catch (e) {
-        console.log(e);
-    }
+    const res = await query(getSubscriptionsQuery, [userId]);
+    return res.rows;
 }
 
 async function addUserSubscription(userId, subscription) {
     const insertFeedQuery = 
     `
-    INSERT INTO feeds (feed_url, icon_url, title)
+    INSERT INTO feeds (feedUrl, iconUrl, title)
     VALUES ($1, $2, $3)
-    ON CONFLICT (feed_url) DO NOTHING;
+    ON CONFLICT (feedUrl) DO NOTHING;
     `;
     const getFeedIdQuery=
     `
-    SELECT feed_id
+    SELECT id
     FROM feeds
-    WHERE feed_url = $1;
+    WHERE feedUrl = $1;
     `;
     const insertSubscriptionQuery =
     `
-    INSERT INTO subscriptions (user_id, feed_id)
+    INSERT INTO subscriptions (userId, feedId)
     VALUES ($1, $2);
     `;
-    try {
-        await query(insertFeedQuery, [
-            subscription.feedUrl,
-            subscription.iconUrl,
-            subscription.title
-        ]);
-        const res = await query(getFeedIdQuery, [subscription.feedUrl]);
-        const feedId = res[0].feed_id;
-        await query(insertSubscriptionQuery, [userId, feedId]);
-    } catch (e) {
-        console.log(e);
-    }
+    await query(insertFeedQuery, [
+        subscription.feedUrl,
+        subscription.iconUrl,
+        subscription.title
+    ]);
+    const res = await query(getFeedIdQuery, [subscription.feedUrl]);
+    const feedId = res.rows[0].id;
+    await query(insertSubscriptionQuery, [userId, feedId]);
 }
 
 async function deleteUserSubscription(userId, subscriptionId) {
     const getFeedIdQuery =
     `
-    SELECT feed_id
+    SELECT feedId
     FROM subscriptions
-    WHERE subscription_id = $1;
+    WHERE id = $1;
     `;
     const deleteSubscriptionQuery =
     `
-    DELETE FROM subscriptions WHERE subscription_id = $1;
+    DELETE FROM subscriptions WHERE id = $1;
     `;
     const deleteFeedQuery =
     `
     DELETE FROM feeds
     WHERE NOT EXISTS (
-        SELECT feed_id
+        SELECT feedId
         FROM subscriptions
-        WHERE feed_id = feeds.feed_id
+        WHERE feedId = feeds.id
     );
     `;
-    try {
-        const res = await query(getFeedIdQuery, [subscriptionId]);
-        const feedId = res[0].feed_id;
-        await query(deleteSubscriptionQuery, [subscriptionId]);
-        await query(deleteFeedQuery, [feedId]);
-    } catch (e) {
-        console.log(e);
-    }
+    const res = await query(getFeedIdQuery, [subscriptionId]);
+    const feedId = res.rows[0].feedId;
+    await query(deleteSubscriptionQuery, [subscriptionId]);
+    await query(deleteFeedQuery, [feedId]);
 }
 
 async function addUser(email, password) {
@@ -117,12 +106,19 @@ async function addUser(email, password) {
     INSERT INTO users (email, password)
     VALUES ($1, $2);
     `
-    try {
-        await query(addUserQuery, [email, password]);
-    } catch (e) {
-        console.log(e);
-    }
+    await query(addUserQuery, [email, password]);
 }
+
+async function getUser(email) {
+    const getUserQuery =
+    `
+    SELECT email, password
+    FROM users
+    WHERE email = $1;
+    `;
+    const res = await query(getUserQuery, [email]);
+    return res[0]
+} 
 
 async function getFeedPosts(subscriptionId) {
     
@@ -138,6 +134,7 @@ module.exports = {
     addUserSubscription,
     deleteUserSubscription,
     addUser,
+    getUser,
     getFeedPosts,
     getAllPosts
 }
